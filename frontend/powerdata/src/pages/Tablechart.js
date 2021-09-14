9be46c26699bc8e25e0b7ce591841711e9045231
Vinay <vinay061@gmail.com>
2021-09-14 16:12:59 +0200

    tablechart

---
+++ frontend/powerdata/src/pages/Tablechart.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Line } from 'react-chartjs-2'
+
+export default function Tablechart(props){
+
+    console.log(`In the child section: ${props}`)
+    const chartvalues = props.data
+    console.log(chartvalues)
+    const values = chartvalues.slice(0,100)
+    console.log(values)
+    let year = []
+    let temp = []
+    values.forEach(elt =>{
+        year.push(elt.Year)
+        temp.push(elt.Glob)
+    })
+    console.log(`Year data: ${year}`)
+    console.log(`Temp data: ${temp}`)
+    
+    
+    return(
+        <div>
+            <p>Chart of the table (child of table)</p>
+            <Line data={
+                {
+                    labels: year,
+                    datasets: [{
+                        label: 'Global temp',
+                        data: temp,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                }
+            }/>
+        </div>
+    )
+}
--- frontend/powerdata/src/pages/Tabledata.js
+++ frontend/powerdata/src/pages/Tabledata.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Table } from 'react-bootstrap'
+import Tablechart from './Tablechart'
 
 export default function Tabledata() {
 
@@ -47,6 +48,7 @@ export default function Tabledata() {
                         }
                     </tbody>
             </Table>
+            <Tablechart data={tableData}/>
         </div>
     )
 }
